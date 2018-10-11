2b16952ef1567af85c15d4c988f141196677d778
panzheng <panzheng@baidu.com>
2018-10-12 03:47:40 +0800

    加上判断条件防止多次渲染

--- src/App.js
+++ src/App.js
@@ -39,30 +39,32 @@ class App extends Component {
         </Tabs>
         <div className="kityminder-core-container" 
           ref={(input) => {
-            this.minder = new window.kityminder.Minder({
-              renderTo: input
-            });
-            this.minder.importJson({
-              "root": {
-                  "data": {
-                      "text": "百度产品",
-                      "image": "https://www.baidu.com/img/bd_logo1.png?where=super",
-                      "imageSize": { "width": 270, "height": 129 }
-                  },
-                  "children": [
-                      { "data": { "text": "新闻" } },
-                      { "data": { "text": "网页", "priority": 1 } },
-                      { "data": { "text": "贴吧", "priority": 2 } },
-                      { "data": { "text": "知道", "priority": 2 } },
-                      { "data": { "text": "音乐", "priority": 3 } },
-                      { "data": { "text": "图片", "priority": 3 } },
-                      { "data": { "text": "视频", "priority": 3 } },
-                      { "data": { "text": "地图", "priority": 3 } },
-                      { "data": { "text": "百科", "priority": 3 } },
-                      { "data": { "text": "更多", "hyperlink": "http://www.baidu.com/more" } }
-                  ]
-              }
-            });
+            if (!this.minder) {
+              this.minder = new window.kityminder.Minder({
+                renderTo: input
+              });
+              this.minder.importJson({
+                "root": {
+                    "data": {
+                        "text": "百度产品",
+                        "image": "https://www.baidu.com/img/bd_logo1.png?where=super",
+                        "imageSize": { "width": 270, "height": 129 }
+                    },
+                    "children": [
+                        { "data": { "text": "新闻" } },
+                        { "data": { "text": "网页", "priority": 1 } },
+                        { "data": { "text": "贴吧", "priority": 2 } },
+                        { "data": { "text": "知道", "priority": 2 } },
+                        { "data": { "text": "音乐", "priority": 3 } },
+                        { "data": { "text": "图片", "priority": 3 } },
+                        { "data": { "text": "视频", "priority": 3 } },
+                        { "data": { "text": "地图", "priority": 3 } },
+                        { "data": { "text": "百科", "priority": 3 } },
+                        { "data": { "text": "更多", "hyperlink": "http://www.baidu.com/more" } }
+                    ]
+                }
+              });
+            }
           }}>
         </div>
       </div>
